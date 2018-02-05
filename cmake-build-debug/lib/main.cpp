434e53aaf1eddc1eeec5a58f0b19b0b4b2c26212
Paria <pariamakaremi20@gmail.com>
2018-02-05 17:06:00 -0500

    first commit

---
+++ cmake-build-debug/lib/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by paria on 05/02/18.
+//
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+int main(int argc,char* argv[])
+{
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
